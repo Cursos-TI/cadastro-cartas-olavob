5aff2592d9adc6faaf9dd6b9e04607b07c175a59
Olavo Bilac <137653930+olavob@users.noreply.github.com>
2026-05-27 23:20:28 -0300

    Add city properties input and display functionality

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado;
+  char codigoDaCarta[5];
+  char nomeDaCidade[50];
+  int populacao;
+  float area;
+  float pib;
+  int nDePontosTuristicos;
+  
   // Área para entrada de dados
+  printf("Insira a letra do Estado: \n");
+  scanf("%c", &estado);
+  
+  printf("Insira o código da carta: \n");
+  scanf("%s", codigoDaCarta);
+  
+  printf("Insira o nome da cidade: \n");
+  scanf("%s", nomeDaCidade);
+  
+  printf("Insira o número de habitantes: \n");
+  scanf("%d", &populacao);
+  
+  printf("Insira a área da cidade em quilómetros quadrados: \n");
+  scanf("%f", &area);
+  
+  printf("Insira o PIB: \n");
+  scanf("%f", &pib);
+  
+  printf("Insira o numero de pontos turísticos: \n");
+  scanf("%d", &nDePontosTuristicos);
 
   // Área para exibição dos dados da cidade
-
+  printf("\n");
+  printf("Estado: %c\n", estado);
+  printf("Código: %s\n", codigoDaCarta);
+  printf("Nome da Cidade: %s\n", nomeDaCidade);
+  printf("População: %d\n", populacao);
+  printf("Área: %.2f\n", area);
+  printf("PIB: %.2f\n", pib);
+  printf("Número de Pontos Turísticos: %d\n", nDePontosTuristicos);
+    
 return 0;
 } 
